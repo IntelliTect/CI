4b4416ef7490511a7862982965c1f3aba21f826d
Andrew Scott <andrew.scott@intellitect.com>
2017-02-08 16:46:47 -0800

    Restyle w/ Chris

--- GenerateHtml/GenerateHtml/Program.cs
+++ GenerateHtml/GenerateHtml/Program.cs
@@ -30,31 +30,16 @@ namespace GenerateHtml
 
         static string ParseExcelRow(string cell)
         {
-            return cell.Replace("\n", "<br/>");
+            return cell.Replace("\n", "</br>").Replace("\"", "\\\"");
         }
 
-
-        static string GetUrlData(ISheet sheet, int rowNum, int col)
+        static string ParseUrl(ICell cell)
         {
-            StringBuilder sb = new StringBuilder("");
-            for (int i = 0; i < MaxUrlCount; i++)
+            if ((cell == null) || (cell.Hyperlink == null) || (string.IsNullOrWhiteSpace(cell.ToString())))
             {
-
-                try
-                {
-                    ICell cell = sheet.GetRow(rowNum++).Cells[col];
-                    if ((cell == null) || (cell.Hyperlink == null) || (string.IsNullOrWhiteSpace(cell.ToString())))
-                    {
-                        return sb.ToString();
-                    }
-                    sb.AppendLine($@"<a href='{cell.Hyperlink}'> {cell.ToString()}</a><br/>");
-                }
-                catch (System.ArgumentOutOfRangeException)
-                {
-                    return sb.ToString();
-                }
+                return null;
             }
-            return sb.ToString();
+            return $@"<a href='{cell.Hyperlink.Address}'> {cell.ToString()}</a><br/>";
         }
 
         static void Main(string[] args)
@@ -66,20 +51,8 @@ namespace GenerateHtml
             }
             Console.WriteLine("using excel file: " + excelFile);
 
-            string header = @"<html xmlns = ""http://www.w3.org/1999/xhtml"">
-<head>
-    <meta http-equiv = ""Content-Type"" content=""text / html; charset = UTF-8"" />
-                   <title> Product Comparison</title>
-                      <link href = 'https://fonts.googleapis.com/css?family=Lato:400,700' rel = 'stylesheet' type = 'text/css' >
-                      <link rel=""stylesheet"" href=""css/normalize.min.css"" >
-                      <link rel=""stylesheet"" href=""css/animate.min.css"" >
-                      <link rel=""stylesheet"" href=""css/comparison.css"" >
-</head>
-<body>
-    <a id=""compare"" href=""#animatedModal"" disabled class=""compare-products"">Compare Products</a>
-	<div class=""container"">";
-            StringBuilder fileContents = new StringBuilder(header);
 
+            var data = new StringBuilder();
 
             XSSFWorkbook hssfwb;
             using (
@@ -96,104 +69,69 @@ namespace GenerateHtml
                 int lastColNum = firstRow.Cells.Count(cell => !string.IsNullOrWhiteSpace(cell.ToString()));
                 int i = 1;
 
+                int lastRow = sheet.LastRowNum; 
+
+                data.AppendLine("var headers = [");
+
+                for (int row = 0; row < lastRow; row++)
+                {
+                    data.Append('"');
+                    data.Append(ParseExcelRow(sheet.GetRow(row).Cells[0].ToString()));
+                    data.AppendLine("\",");
+                }
+                data.Append("];");
+                data.AppendLine();
+                data.AppendLine();
+
+                data.Append("var products = [");
 
                 for (int col = 1; col < lastColNum; col++) //start at 1 to skip over labels
                 {
-                    int rowNum = 0;
-                    StringBuilder sb = new StringBuilder($@"<div class=""product"" data-id=""{i++}"" ");
-                    // IRow row = sheet.GetRow(rowNum++);
-                    sb.AppendLine($@"data-title=""{ParseExcelRow(sheet.GetRow(rowNum++).Cells[col].ToString())}"" ");
-                    sb.AppendLine($@"data-model=""{ParseExcelRow(sheet.GetRow(rowNum++).Cells[col].ToString())}"" ");
-                    sb.AppendLine($@"data-vendor=""{ParseExcelRow(sheet.GetRow(rowNum++).Cells[col].ToString())}"" ");
-                    sb.AppendLine($@"data-technology=""{ParseExcelRow(sheet.GetRow(rowNum++).Cells[col].ToString())}"" ");
-                    sb.AppendLine($@"data-wavelength=""{ParseExcelRow(sheet.GetRow(rowNum++).Cells[col].ToString())}"" ");
-                    sb.AppendLine($@"data-depth=""{ParseExcelRow(sheet.GetRow(rowNum++).Cells[col].ToString())}"" ");
-                    sb.AppendLine($@"data-power=""{ParseExcelRow(sheet.GetRow(rowNum++).Cells[col].ToString())}"" ");
-                    sb.AppendLine($@"data-film=""{ParseExcelRow(sheet.GetRow(rowNum++).Cells[col].ToString())}"" ");
-                    sb.AppendLine($@"data-soldermask=""{ParseExcelRow(sheet.GetRow(rowNum++).Cells[col].ToString())}"" ");
-                    sb.AppendLine($@"data-speed=""{ParseExcelRow(sheet.GetRow(rowNum++).Cells[col].ToString())}"" ");
-                    sb.AppendLine($@"data-edge=""{ParseExcelRow(sheet.GetRow(rowNum++).Cells[col].ToString())}"" ");
-                    sb.AppendLine($@"data-positional=""{ParseExcelRow(sheet.GetRow(rowNum++).Cells[col].ToString())}"" ");
-                    sb.AppendLine($@"data-registration=""{ParseExcelRow(sheet.GetRow(rowNum++).Cells[col].ToString())}"" ");
-                    sb.AppendLine($@"data-line=""{ParseExcelRow(sheet.GetRow(rowNum++).Cells[col].ToString())}"" ");
-                    string line2Img = ParseExcelRow(sheet.GetRow(rowNum++).Cells[col].ToString());
-                    sb.AppendLine($@"data-line2={GetImage(line2Img)}");
-                    sb.AppendLine($@"data-df=""{ParseExcelRow(sheet.GetRow(rowNum++).Cells[col].ToString())}"" ");
-                    sb.AppendLine($@"data-hole=""{ParseExcelRow(sheet.GetRow(rowNum++).Cells[col].ToString())}"" ");
-                    sb.AppendLine($@"data-hole2=""{ParseExcelRow(sheet.GetRow(rowNum++).Cells[col].ToString())}"" ");
-                    sb.AppendLine($@"data-sro=""{ParseExcelRow(sheet.GetRow(rowNum++).Cells[col].ToString())}"" ");
-                    sb.AppendLine($@"data-sr=""{ParseExcelRow(sheet.GetRow(rowNum++).Cells[col].ToString())}"" ");
-                    sb.AppendLine($@"data-address=""{ParseExcelRow(sheet.GetRow(rowNum++).Cells[col].ToString())}"" ");
-                    sb.AppendLine($@"data-min=""{ParseExcelRow(sheet.GetRow(rowNum++).Cells[col].ToString())}"" ");
-                    sb.AppendLine($@"data-max=""{ParseExcelRow(sheet.GetRow(rowNum++).Cells[col].ToString())}"" ");
-                    sb.AppendLine($@"data-panel=""{ParseExcelRow(sheet.GetRow(rowNum++).Cells[col].ToString())}"" ");
-                    sb.AppendLine($@"data-handling=""{ParseExcelRow(sheet.GetRow(rowNum++).Cells[col].ToString())}"" "); 
-                    sb.AppendLine($@"data-machine=""{ParseExcelRow(sheet.GetRow(rowNum++).Cells[col].ToString())}"" ");
-                    sb.AppendLine($@"data-models=""{ParseExcelRow(sheet.GetRow(rowNum++).Cells[col].ToString())}"" ");
-                    sb.AppendLine($@"data-double=""{ParseExcelRow(sheet.GetRow(rowNum++).Cells[col].ToString())}"" ");
-                    sb.AppendLine($@"data-expected=""{ParseExcelRow(sheet.GetRow(rowNum++).Cells[col].ToString())}"" ");
-                    sb.AppendLine($@"data-warranty=""{ParseExcelRow(sheet.GetRow(rowNum++).Cells[col].ToString())}"" ");
-                    sb.AppendLine($@"data-warranty2=""{ParseExcelRow(sheet.GetRow(rowNum++).Cells[col].ToString())}"" ");
-                    sb.AppendLine($@"data-mechanical=""{ParseExcelRow(sheet.GetRow(rowNum++).Cells[col].ToString())}"" ");
-                    sb.AppendLine($@"data-autofocus=""{ParseExcelRow(sheet.GetRow(rowNum++).Cells[col].ToString())}"" ");
-                    sb.AppendLine($@"data-panel2=""{ParseExcelRow(sheet.GetRow(rowNum++).Cells[col].ToString())}"" ");
-                    sb.AppendLine($@"data-non-linear=""{ParseExcelRow(sheet.GetRow(rowNum++).Cells[col].ToString())}"" ");
-                    sb.AppendLine($@"data-url=""{GetUrlData(sheet, rowNum, col)}""");
-                    rowNum += MaxUrlCount;
-                        //                    sb.AppendLine($@"data-url=""<a href='{ParseExcelRow(sheet.GetRow(rowNum++).Cells[col].ToString())}'>Click Here</a>""");
-                    sb.AppendLine($@"data-notes=""{ParseExcelRow(sheet.GetRow(rowNum++).Cells[col].ToString())}""");
-                    sb.AppendLine($@"data-video=""<a href='{ParseExcelRow(sheet.GetRow(rowNum++).Cells[col].ToString())}'>Click Here</a>""");
-                    sb.AppendLine(@">");
-                    sb.AppendLine($@"<button><div>+</div></button>");
-                    sb.AppendLine($@"<img src = ""products/{sheet.GetRow(26).Cells[col]}"" onerror=""this.src='products/none.png'"" width=""250px"" height=""230px"" class=""product-image"" />");
-                    sb.AppendLine($@"<h3 class=""text-center"">{ParseExcelRow(sheet.GetRow(0).Cells[col].ToString())}</h3>"); //machine
-                    sb.AppendLine($@"<p class=""description"">{ParseExcelRow(sheet.GetRow(2).Cells[col].ToString())}</p>"); //vendor
- //                   sb.AppendLine($@"<p class=""description"">{sheet.GetRow(3).Cells[col]}</p>"); //technology
-                    sb.AppendLine("</div>");
-                    fileContents.Append(sb);
+                    data.Append("{ checked: ko.observable(false), data: [");
+
+                    for (int row = 0; row < lastRow; row++)
+                    {
+                        //var rowName = sheet.GetRow(row).Cells[0].ToString();
+                        //if (rowName == "URL")
+                        //{
+
+                        //}
+
+                        data.Append('"');
+
+                        var cell = sheet.GetRow(row).GetCell(col);
+                        if (cell == null)
+                        {
+                            // output nothing
+                        }
+                        else
+                        {
+                            var url = ParseUrl(cell);
+                            if (url != null)
+                            {
+                                data.Append(url);
+                            }
+                            else
+                            {
+                                var contents = ParseExcelRow(cell.ToString());
+                                data.Append(contents);
+                            }
+                        }
+
+                        data.AppendLine("\",");
+                    }
+
+                    data.AppendLine("]},");
                 }
+
+                data.AppendLine("];");
+
+                data.AppendLine(@"
+                    model = {headers: headers, products: products};
+                ");
             }
 
-
-            string footer = @"<div id= ""animatedModal"">
-					<div id = ""btn-close-modal"" class=""close-animatedModal"">
-						CLOSE
-                    </div>
-					<div class=""modal-content"">
-						<div class=""modal-inner"">
-							<div class=""no-products"">Select some products to compare first</div>
-							<div class=""modal-products""></div>
-						</div>
-					</div>
-				</div>
-				</div>
-</body>
-<script type = ""text/javascript"" src=""https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js""></script>
-<script type = ""text/javascript"" src=""js/jquery.comparison.js""></script>
-<script type = ""text/javascript"" src=""js/animatedModal.js""></script>
-<script>
-	$(""#compare"").animatedModal({
-
-		animatedIn: 'lightSpeedIn',
-		animatedOut: 'bounceOutDown',
-		color: '#3498db',
-
-	});
-
-	$(document).ready(function ()
-    {
-
-		$('.product').compare({
-            compareButton: '.compare-products'
-
-        });
-
-
-    });
-</script>
-</html>";
-            fileContents.Append(footer);
-            File.WriteAllText(@"..\..\..\..\index.html", fileContents.ToString());
+            File.WriteAllText(@"..\..\..\..\data.js", data.ToString());
         }
     }
 }
--- js/jquery.comparison.js
+++ js/jquery.comparison.js
@@ -12,67 +12,67 @@
 		// if the compare button is clicked add the items to the
 		// modal window - opening the modal is triggered by the modal
 		// plugin seperately
-		$(options.compareButton).click(function () {
+		// $(options.compareButton).click(function () {
 
-			// if the button has the class active we have items to compare
-			if ($(this).hasClass('active')) {
+		// 	// if the button has the class active we have items to compare
+		// 	if ($(this).hasClass('active')) {
 
-				$('.modal-products').empty(); // clear the modal
-				$('.no-products').hide(); // hide the no products selected message
+		// 		$('.modal-products').empty(); // clear the modal
+		// 		$('.no-products').hide(); // hide the no products selected message
 
-				var arrayLength = itemsarray.length;
-				// loop through the array if products
-				for (var i = 0; i < arrayLength; i++) {
+		// 		var arrayLength = itemsarray.length;
+		// 		// loop through the array if products
+		// 		for (var i = 0; i < arrayLength; i++) {
 
-					product = $('.product[data-id="' + itemsarray[i] + '"]');
+		// 			product = $('.product[data-id="' + itemsarray[i] + '"]');
 
-					$('.modal-products').append('<div class="product"><img height="230p" width="250px" src="' + $(product).find('img').attr('src') +
-						'"/><ul><li><span>' + $(product).data('title') + '<br/>' + $(product).data('url') +
-						'</span></li>' +
-						'<li><span><span class="productHeader">Model<br></span>' + $(product).data('model') + '</span></li>' +
-						'<li><span><span class="productHeader">Technology:<br/></span>' + $(product).data('technology') + '</span></li>' +
-						'</span></li><li><span><span class="productHeader">Depth of Focus:<br/></span>' + $(product).data('depth') + '</span></li>' +
-						'</span></li><li><span><span class="productHeader">Power:<br/></span>' + $(product).data('power') + '</span></li>' +
-						'</span></li><li><span><span class="productHeader">Film Resist Sides/hour:<br/></span>' + $(product).data('film') + '</span></li>' +
-						'</span></li><li><span><span class="productHeader">Soldermask Sides/hour:<br/></span>' + $(product).data('soldermask') + '</span></li>' +
-						'</span></li><li><span><span class="productHeader">Speed Details:<br/></span>' + $(product).data('speed') + '</span></li>' +
-						'</span></li><li><span><span class="productHeader">Edge Roughness:<br/></span>' + $(product).data('edge') + '</span></li>' +
-						'</span></li><li><span><span class="productHeader">Positional,Data, and Alignment Accuracy:<br/></span>' + $(product).data('positional') + '</span></li>' +
-						'</span></li><li><span><span class="productHeader">Registration Accuracy (Side by Side Alignment Accuracy):<br/></span>' + $(product).data('registration') + '</span></li>' +
-						'</span></li><li><span><span class="productHeader">Line/Space Width:<br/></span>' + $(product).data('line') + '</span></li>' +
-						'</span></li><li><span><span class="productHeader">Line/Space SEM Image:<br/></span>' + $(product).data('line2') + '</span></li>' +
-						'</span></li><li><span><span class="productHeader">DF Thickness:<br/></span>' + $(product).data('df') + '</span></li>' +
-						'</span></li><li><span><span class="productHeader">Hole Diameter:<br/></span>' + $(product).data('hole') + '</span></li>' +
-						'</span></li><li><span><span class="productHeader">Hole Diameter Image:<br/></span>' + $(product).data('hole2') + '</span></li>' +
-						'</span></li><li><span><span class="productHeader">Solder Resist Opening:<br/></span>' + $(product).data('sro') + '</span></li>' +
-						'</span></li><li><span><span class="productHeader">SR Thickness:<br/></span>' + $(product).data('sr') + '</span></li>' +
-						'</span></li><li><span><span class="productHeader">Address Pitch:<br/></span>' + $(product).data('address') + '</span></li>' +
-						'</span></li><li><span><span class="productHeader">Min. Image Size:<br/></span>' + $(product).data('min') + '</span></li>' +
-						'</span></li><li><span><span class="productHeader">Max. Image Size:<br/></span>' + $(product).data('max') + '</span></li>' +
-						'</span></li><li><span><span class="productHeader">Panel Thickness:<br/></span>' + $(product).data('panel') + '</span></li>' +
-						'</span></li><li><span><span class="productHeader">Handling Options:<br/></span>' + $(product).data('handling') + '</span></li>' +
-						'</span></li><li><span><span class="productHeader">Machine Dimensions:<br/></span>' + $(product).data('machine') + '</span></li>' +
-						//'</span></li><li><span><span class="productHeader">Models:<br/></span>'+$(product).data('models')+'</span></li>'+
-						'</span></li><li><span><span class="productHeader">Double Machine Images:<br/></span>' + $(product).data('double') + '</span></li>' +
-						'</span></li><li><span><span class="productHeader">Expected Laser DMD Head Lifetime:<br/></span>' + $(product).data('expected') + '</span></li>' +
-						'</span></li><li><span><span class="productHeader">Warranty Standard:<br/></span>' + $(product).data('warranty') + '</span></li>' +
-						'</span></li><li><span><span class="productHeader">Warranty Cost/year:<br/></span>' + $(product).data('warranty2') + '</span></li>' +
-						'</span></li><li><span><span class="productHeader">Mechanical Clamping:<br/></span>' + $(product).data('mechanical') + '</span></li>' +
-						'</span></li><li><span><span class="productHeader">Autofocus if needed:<br/></span>' + $(product).data('autofocus') + '</span></li>' +
-						'</span></li><li><span><span class="productHeader">Panel Traceability (Serial Number Tracking):<br/></span>' + $(product).data('panel2') + '</span></li>' +
-						'</span></li><li><span><span class="productHeader">Non-linear Distortion:<br/></span>' + $(product).data('non-linear') + '</span></li>' +
-						//'</span></li><li><span>URL:<br/></span>'+$(product).data('url')+'</span></li>'+
-						'</span></li><li><span><span class="productHeader">Video Examples:<br/></span>' + $(product).data('video') + '</span></li>' +
-						'</span></li><li><span><span class="productHeader">Notes:<br/></span>' + $(product).data('notes') + '</span></li></div>' +
-						'</span></li></ul></div>');
-				}
+		// 			$('.modal-products').append('<div class="product"><img height="230p" width="250px" src="' + $(product).find('img').attr('src') +
+		// 				'"/><ul><li><span>' + $(product).data('title') + '<br/>' + $(product).data('url') +
+		// 				'</span></li>' +
+		// 				'<li><span><span class="productHeader">Model<br></span>' + $(product).data('model') + '</span></li>' +
+		// 				'<li><span><span class="productHeader">Technology:<br/></span>' + $(product).data('technology') + '</span></li>' +
+		// 				'</span></li><li><span><span class="productHeader">Depth of Focus:<br/></span>' + $(product).data('depth') + '</span></li>' +
+		// 				'</span></li><li><span><span class="productHeader">Power:<br/></span>' + $(product).data('power') + '</span></li>' +
+		// 				'</span></li><li><span><span class="productHeader">Film Resist Sides/hour:<br/></span>' + $(product).data('film') + '</span></li>' +
+		// 				'</span></li><li><span><span class="productHeader">Soldermask Sides/hour:<br/></span>' + $(product).data('soldermask') + '</span></li>' +
+		// 				'</span></li><li><span><span class="productHeader">Speed Details:<br/></span>' + $(product).data('speed') + '</span></li>' +
+		// 				'</span></li><li><span><span class="productHeader">Edge Roughness:<br/></span>' + $(product).data('edge') + '</span></li>' +
+		// 				'</span></li><li><span><span class="productHeader">Positional,Data, and Alignment Accuracy:<br/></span>' + $(product).data('positional') + '</span></li>' +
+		// 				'</span></li><li><span><span class="productHeader">Registration Accuracy (Side by Side Alignment Accuracy):<br/></span>' + $(product).data('registration') + '</span></li>' +
+		// 				'</span></li><li><span><span class="productHeader">Line/Space Width:<br/></span>' + $(product).data('line') + '</span></li>' +
+		// 				'</span></li><li><span><span class="productHeader">Line/Space SEM Image:<br/></span>' + $(product).data('line2') + '</span></li>' +
+		// 				'</span></li><li><span><span class="productHeader">DF Thickness:<br/></span>' + $(product).data('df') + '</span></li>' +
+		// 				'</span></li><li><span><span class="productHeader">Hole Diameter:<br/></span>' + $(product).data('hole') + '</span></li>' +
+		// 				'</span></li><li><span><span class="productHeader">Hole Diameter Image:<br/></span>' + $(product).data('hole2') + '</span></li>' +
+		// 				'</span></li><li><span><span class="productHeader">Solder Resist Opening:<br/></span>' + $(product).data('sro') + '</span></li>' +
+		// 				'</span></li><li><span><span class="productHeader">SR Thickness:<br/></span>' + $(product).data('sr') + '</span></li>' +
+		// 				'</span></li><li><span><span class="productHeader">Address Pitch:<br/></span>' + $(product).data('address') + '</span></li>' +
+		// 				'</span></li><li><span><span class="productHeader">Min. Image Size:<br/></span>' + $(product).data('min') + '</span></li>' +
+		// 				'</span></li><li><span><span class="productHeader">Max. Image Size:<br/></span>' + $(product).data('max') + '</span></li>' +
+		// 				'</span></li><li><span><span class="productHeader">Panel Thickness:<br/></span>' + $(product).data('panel') + '</span></li>' +
+		// 				'</span></li><li><span><span class="productHeader">Handling Options:<br/></span>' + $(product).data('handling') + '</span></li>' +
+		// 				'</span></li><li><span><span class="productHeader">Machine Dimensions:<br/></span>' + $(product).data('machine') + '</span></li>' +
+		// 				//'</span></li><li><span><span class="productHeader">Models:<br/></span>'+$(product).data('models')+'</span></li>'+
+		// 				'</span></li><li><span><span class="productHeader">Double Machine Images:<br/></span>' + $(product).data('double') + '</span></li>' +
+		// 				'</span></li><li><span><span class="productHeader">Expected Laser DMD Head Lifetime:<br/></span>' + $(product).data('expected') + '</span></li>' +
+		// 				'</span></li><li><span><span class="productHeader">Warranty Standard:<br/></span>' + $(product).data('warranty') + '</span></li>' +
+		// 				'</span></li><li><span><span class="productHeader">Warranty Cost/year:<br/></span>' + $(product).data('warranty2') + '</span></li>' +
+		// 				'</span></li><li><span><span class="productHeader">Mechanical Clamping:<br/></span>' + $(product).data('mechanical') + '</span></li>' +
+		// 				'</span></li><li><span><span class="productHeader">Autofocus if needed:<br/></span>' + $(product).data('autofocus') + '</span></li>' +
+		// 				'</span></li><li><span><span class="productHeader">Panel Traceability (Serial Number Tracking):<br/></span>' + $(product).data('panel2') + '</span></li>' +
+		// 				'</span></li><li><span><span class="productHeader">Non-linear Distortion:<br/></span>' + $(product).data('non-linear') + '</span></li>' +
+		// 				//'</span></li><li><span>URL:<br/></span>'+$(product).data('url')+'</span></li>'+
+		// 				'</span></li><li><span><span class="productHeader">Video Examples:<br/></span>' + $(product).data('video') + '</span></li>' +
+		// 				'</span></li><li><span><span class="productHeader">Notes:<br/></span>' + $(product).data('notes') + '</span></li></div>' +
+		// 				'</span></li></ul></div>');
+		// 		}
 
-				// else we haven't selected any products to compare yet
-			} else {
-				$('.modal-products').empty(); // clear the modal
-				$('.no-products').show(); // show the no products selected message       	
-			}
-		});
+		// 		// else we haven't selected any products to compare yet
+		// 	} else {
+		// 		$('.modal-products').empty(); // clear the modal
+		// 		$('.no-products').show(); // show the no products selected message       	
+		// 	}
+		// });
 
 
 		// If an item is click for comparison run this code
